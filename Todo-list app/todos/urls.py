c4898fd083540e445ad769af7ae21bb82eecd53c
Minh Thinh <22280087@student.hcmus.edu.vn>
2025-01-25 16:47:07 +0700

    update todo app - done

--- Todo-list app/todos/urls.py
+++ Todo-list app/todos/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.todo_list, name="todo_list"),  # Đường dẫn gốc cho danh sách todos
-    path("add/", views.add_todo, name="add_todo"),  # Đường dẫn thêm mới todos
+    # path("add/", views.add_todo, name="add_todo"),  # Đường dẫn thêm mới todos
 ]
